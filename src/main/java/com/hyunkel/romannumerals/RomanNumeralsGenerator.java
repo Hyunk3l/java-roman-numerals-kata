92487bd9d37293d3183f1cfd8f94f6e626a7b26f
Fabri Di Napoli <hyunk3l@gmail.com>
2017-07-12 21:28:38 +0200

    Add Romal Numerals TDD Kata Part I

--- src/main/java/com/hyunkel/romannumerals/RomanNumeralsGenerator.java
+++ src/main/java/com/hyunkel/romannumerals/RomanNumeralsGenerator.java
@@ -3,6 +3,37 @@ package com.hyunkel.romannumerals;
 public class RomanNumeralsGenerator {
 
     public static String romanFor(int decimal) {
-        return null;
+        String roman = "";
+        for (RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
+            while(decimal >= romanToDecimal.decimal) {
+                roman += romanToDecimal.roman;
+                decimal -= romanToDecimal.decimal;
+            }
+        }
+        return roman;
+    }
+
+    enum RomanToDecimal {
+        THOUSAND("M", 1000),
+        NINE_HUNDRED("CM", 900),
+        FIVE_HUNDRED("D", 500),
+        FOUR_HUNDRED("CD", 400),
+        HUNDRED("C", 100),
+        NINETY("XC", 90),
+        FIFTY("L", 50),
+        FORTY("XL", 40),
+        TEN("X", 10),
+        NINE("IX", 9),
+        FIVE("V", 5),
+        FOUR("IV", 4),
+        ONE("I", 1);
+
+        private final String roman;
+        private final int decimal;
+
+        RomanToDecimal(String roman, int decimal) {
+            this.roman = roman;
+            this.decimal = decimal;
+        }
     }
 }
--- src/test/java/com/hyunkel/romannumerals/RomanNumeralsGeneratorShould.java
+++ src/test/java/com/hyunkel/romannumerals/RomanNumeralsGeneratorShould.java
@@ -9,6 +9,17 @@ public class RomanNumeralsGeneratorShould {
 
     @Test
     public void generate_a_roman_number_for_a_given_decimal_number() {
-        assertThat(romanFor(1)).isSameAs("I");
+        assertThat(romanFor(1)).isEqualTo("I");
+        assertThat(romanFor(2)).isEqualTo("II");
+        assertThat(romanFor(3)).isEqualTo("III");
+        assertThat(romanFor(4)).isEqualTo("IV");
+        assertThat(romanFor(5)).isEqualTo("V");
+        assertThat(romanFor(7)).isEqualTo("VII");
+        assertThat(romanFor(9)).isEqualTo("IX");
+        assertThat(romanFor(10)).isEqualTo("X");
+        assertThat(romanFor(18)).isEqualTo("XVIII");
+        assertThat(romanFor(30)).isEqualTo("XXX");
+        assertThat(romanFor(2687)).isEqualTo("MMDCLXXXVII");
+        assertThat(romanFor(3499)).isEqualTo("MMMCDXCIX");
     }
 }
