0986a84c02d428394650f6dbe1dea5006279cead
Fabri Di Napoli <hyunk3l@gmail.com>
2017-07-13 21:35:00 +0200

    Solve part-two of the kata - from roman to decimal

--- src/main/java/com/hyunkel/romannumerals/RomanNumeralsGenerator.java
+++ src/main/java/com/hyunkel/romannumerals/RomanNumeralsGenerator.java
@@ -14,7 +14,17 @@ public class RomanNumeralsGenerator {
     }
 
     public static int decimalFor(String roman) {
-        return 1;
+        int decimal = 0;
+
+        while (!roman.isEmpty()) {
+            for(DecimalToRoman decimalToRoman : DecimalToRoman.values()) {
+                if(roman.contains(decimalToRoman.roman)) {
+                    roman = roman.replaceFirst(decimalToRoman.roman, "");
+                    decimal += decimalToRoman.decimal;
+                }
+            }
+        }
+        return decimal;
     }
 
     enum RomanToDecimal {
@@ -36,6 +46,30 @@ public class RomanNumeralsGenerator {
         private final int decimal;
 
         RomanToDecimal(String roman, int decimal) {
+            this.roman = roman;
+            this.decimal = decimal;
+        }
+    }
+
+    enum DecimalToRoman {
+        NINE_HUNDRED("CM", 900),
+        THOUSAND("M", 1000),
+        FOUR_HUNDRED("CD", 400),
+        FIVE_HUNDRED("D", 500),
+        NINETY("XC", 90),
+        HUNDRED("C", 100),
+        FORTY("XL", 40),
+        FIFTY("L", 50),
+        NINE("IX", 9),
+        TEN("X", 10),
+        FOUR("IV", 4),
+        FIVE("V", 5),
+        ONE("I", 1);
+
+        private final int decimal;
+        private final String roman;
+
+        DecimalToRoman(String roman, int decimal) {
             this.roman = roman;
             this.decimal = decimal;
         }
